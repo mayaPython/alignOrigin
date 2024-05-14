8ccf6526f3cc3793d9e6a0a216cd9cab9b57208a
mayaPython <mayaPython@users.noreply.github.com>
2024-05-14 08:08:36 +0100

    Updated for 2023

--- alignOrigin.py
+++ alignOrigin.py
@@ -2,34 +2,38 @@
 
 written by Emily Pollacchi
   	file name alignOrigin
-  	Copyright (C) 2016 by Emily Pollacchi
+  	Copyright (C) 2024 by Emily Pollacchi
   	epollacchi@gmail.com
 
 '''
 
-import maya.cmds as mc
+import maya.cmds as cmds
 
 def alignOrigin():
 
     #Select objects
-    selObjs = mc.ls(selection=True)
+    selObjs = cmds.ls(selection=True)
+	
+    #Check for selection
+    if not selObjs:
+        cmds.error("No objects selected.")
     
     #move pivot to the bottom
     for Objs in selObjs:
-        bbox = mc.exactWorldBoundingBox(Objs)
+        bbox = cmds.exactWorldBoundingBox(Objs)
         bottom = [(bbox[0] + bbox[3])/2, bbox[1], (bbox[2] + bbox[5])/2]
-        mc.xform(Objs, piv=bottom, ws=True)
+        cmds.xform(Objs, piv=bottom, ws=True)
     
     #move object to the origin
     for Objs in selObjs:
-        mc.move(0,0,0, Objs, rpr=True, absolute=True)
+        cmds.move(0,0,0, Objs, rpr=True, absolute=True)
         
     #freeze transformations
     for Objs in selObjs:
-        mc.makeIdentity(apply=True, t=1, r=1, s=1, n=0)
+        cmds.makeIdentity(apply=True, t=1, r=1, s=1, n=0)
         
     #delete non-deformer history
     for Objs in selObjs:
-        mc.BakeNonDefHistory()
+        cmds.BakeNonDefHistory()
 
 alignOrigin()
